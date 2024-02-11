b1c42fd73b6af79d33f0991431ac72c011410cd5
Florian Guerin <guerinflorian02@gmail.com>
2024-02-11 16:52:15 +0100

    new app + tailwind + banner

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,86 @@
+import './Banner.css'
+import React, { useState } from 'react';
+
+
+function Banner() {
+  return (
+    <div className='flex justify-between'>
+      <ScrollToTop />
+      <div className='flex justify-around items-center'>
+        <ScrollToProjects />
+        <ScrollToContact />
+        <div>
+          <MyDropdown />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const MyDropdown = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <select id="dropdown" className='bg-transparent' value={selectedOption} onChange={handleOptionChange}>
+        <option value="option1">EN</option>
+        <option value="option2">FR</option>
+        <option value="option3">ES</option>
+      </select>
+
+    </div>
+  );
+};
+
+const ScrollToProjects = () => {
+  const scrollToProjects = () => {
+    const viewportHeight = window.innerHeight || document.documentElement.clientHeight
+    window.scrollTo({
+      top: viewportHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className='mx-2'>
+      <button onClick={scrollToProjects}><p>Projects</p></button>
+    </div>
+  );
+};
+
+const ScrollToContact = () => {
+  const scrollToContact = () => {
+    const viewportHeight = window.innerHeight || document.documentElement.clientHeight
+    window.scrollTo({
+      top: 2 * viewportHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className='mx-2'>
+      <button onClick={scrollToContact}><p>Contact</p></button>
+    </div>
+  );
+};
+
+const ScrollToTop = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={scrollToTop}><h1>FLO</h1></button>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import Banner from '../Banner/Banner';
+import './Home.css'
+
+
+function Home() {
+  return (
+    <div className='container mx-auto mt-3'>
+      <Banner />
+    </div>
+  );
+}
+
+export default Home
